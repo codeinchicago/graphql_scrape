88aea71a895cbc33bdeb5518008287d286b51e9b
codeinchicago <vancewcole@gmail.com>
2022-07-25 14:48:15 -0500

    first commit

---
+++ schema.py
@@ -0,0 +1,26 @@
+import graphene
+import extraction
+import requests
+
+def extract(url):
+    html = requests.get(url).text
+    extracted = extraction.Extractor().extract(html, source_url=url)
+    return extracted
+
+class Website(graphene.ObjectType):
+    url = graphene.String(required=True)
+    title = graphene.String()
+    description = graphene.String()
+    image = graphene.String()
+
+class Query(graphene.ObjectType):
+    website = graphene.Field(Website, url=graphene.String())
+
+    def resolve_website(self, info, url):
+        extracted = extract(url)
+        return Website(url=url,
+                       title=extracted.title,
+                       description=extracted.description,
+                       image=extracted.image)
+
+schema = graphene.Schema(query=Query)
